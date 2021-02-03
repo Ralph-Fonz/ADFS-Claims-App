687776070b1859a301be276cb79ad2c170960f4e
Ralph_Fonz <raphael@fonseca.tech>
2021-02-03 14:19:12 -0500

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ADFS_Claims_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
